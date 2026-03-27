63bb8776498dee2c750de0f627d6aac4dcc2d48a
Priyanshu Gawate <159117047+priyanshu052004@users.noreply.github.com>
2026-03-27 17:06:13 +0530

    [Update] Today i learn How to Create Api and test by Postman

--- section7/src/main/java/com/s2p/section7/Controller/HelloController.java
+++ section7/src/main/java/com/s2p/section7/Controller/HelloController.java
@@ -1,14 +1,30 @@
 package com.s2p.section7.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
 
-    @GetMapping(path = {"/","/hello"})
-    public String hello(){
-        return "Hello priyanshu!";
+    @GetMapping(path = {"/api/v1/get"})
+    public String getApiMethod(){
+        return "Get Api Method" ;
+    }
+    @PutMapping(path = {"/api/v1/put"})
+    public String putApiMethod(){
+        return "put Api Method" ;
+    }
+    @DeleteMapping (path = {"/api/v1/delete"})
+    public String deleteApiMethod(){
+        return "delete Api Method" ;
+    }
+    @PostMapping(path = {"/api/v1/post"})
+    public String postApiMethod(){
+        return "post Api Method" ;
+    }
+    @PatchMapping(path = {"/api/v1/patch"})
+    public String patchApiMethod(){
+        return "patch Api Method" ;
     }
 }
